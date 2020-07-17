9357d2f11e0dbdccb510d550f5ecc119c8a055fd
Rob Olague <robolague@users.noreply.github.com>
2020-07-16 18:12:07 -0700

    Add files via upload

--- RenClock.ino
+++ RenClock.ino
@@ -9,14 +9,22 @@ RTC_DS1307 rtc;
 #define DATA_PIN 5
 CRGB leds[NUM_LEDS];
 
+const byte switchPin = 3; // momentary switch on 3, other side connected to ground, 2&3 are interrupt pins
+volatile byte state = LOW;
+bool override = false; //override is off
 
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 void setup () {
-  delay(2000);
+  pinMode(switchPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(switchPin),interruptbutton,CHANGE);
+  
+  //digitalWrite(switchPin, HIGH);  // turn on pullup resistor
+  //delay(2000);
   //FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
   FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);  // GRB ordering is typical
-  fill_solid(leds, NUM_LEDS, CRGB::Black);
+  fill_solid(leds, NUM_LEDS, CRGB::Green);
+  FastLED.setBrightness(50);
   FastLED.show();
 
   while (!Serial); // for Leonardo/Micro/Zero
@@ -32,12 +40,20 @@ void setup () {
     // following line sets the RTC to the date & time this sketch was compiled
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+    // July 16, 2020 at 7am you would call:
+    // rtc.adjust(DateTime(2020, 7, 16, 7, 0, 0));
   }
+    //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 }
 
 void loop () {
+
+  if (override == true) {
+    Serial.print("We are in override mode! All systems RED");
+    fill_solid(leds, NUM_LEDS, CRGB::Red);
+    FastLED.setBrightness(50);
+    FastLED.show();
+  }
   DateTime now = rtc.now();
 
 //  Serial.print(now.year(), DEC);
@@ -61,16 +77,16 @@ void loop () {
 //  }
 
 //Nightime 
-  if ((now.hour() == 19 && now.minute() == 30) || (now.hour() >= 20) || (now.hour() <=7)) {
-    // hour is 7:30pm OR it's past 8:00pm OR it's before 7am
-    Serial.print("It's past 7:30pm but before 7:00am");
+  if (override == false && (now.hour() == 19 && now.minute() == 30) || (now.hour() >= 20) || (now.hour() <=7) && now.minute() < 30) {
+    // hour is 7:30pm OR it's past 8:00pm OR it's before 7:30am
+    Serial.print("It's past 7:30pm but before 7:30am");
     fill_solid(leds, NUM_LEDS, CRGB::Red);
     FastLED.setBrightness(50);
     FastLED.show();
   }
 
 //Morning
-  if ((now.hour() == 7 && now.minute() == 30) || (now.hour() >= 8 && now.hour() < 13)) {
+  if (override == false && (now.hour() == 7 && now.minute() == 30) || (now.hour() >= 8 && now.hour() < 13)) {
     // hour is 7:30am OR (it's past 8am AND it's before 1pm)
     Serial.print("It's past 7:30am and before 1pm");
     fill_solid(leds, NUM_LEDS, CRGB::Green);
@@ -79,7 +95,7 @@ void loop () {
   }
 
   //Naptime
-  if ((now.hour() >= 13 && now.hour() < 14) || (now.hour() ==14 && now.minute() <= 30) ) {
+  if (override == false && (now.hour() >= 13 && now.hour() < 14) || (now.hour() ==14 && now.minute() <= 30) ) {
     // hour is 1:00pm or it's 2:00-2:30
     Serial.print("It's past 1pm or between 2:00pm and 2:30pm");
     fill_solid(leds, NUM_LEDS, CRGB::Red);
@@ -88,14 +104,16 @@ void loop () {
   }
 
   //Afternoon
-  if ((now.hour() == 14 && now.minute() == 30) || (now.hour() > 14 && now.hour() < 19)) {
+  if (override == false && (now.hour() == 14 && now.minute() == 30) || (now.hour() > 14 && now.hour() < 19)) {
     // hour is 2:30pm OR (it's past 2pm AND it's before 7pm)
-    Serial.print("It's past 2:30pm and before 1pm");
+    Serial.print("It's past 2:30pm and before 7pm");
     fill_solid(leds, NUM_LEDS, CRGB::Green);
     FastLED.setBrightness(50);
     FastLED.show();
   }
 
+  
+  //delay(3000); // only update every 3 seconds
   delay(60000); // only update every 1 minute
 
 //just tests that colors are being sent appropriately
@@ -110,4 +128,12 @@ void loop () {
 
   Serial.println("\nStill working");
 
+}
+
+void interruptbutton() {
+  if (digitalRead(switchPin) == LOW) {
+  // switch is pressed - pullup keeps pin high normally
+  delay(100);                     // delay to debounce switch
+  override = !override;             // toggle running variable
+  }
 }
